115932be0f2bd6b5470a042201d376e6085b5d9c
Duong Pham <duongpham17@icloud.com>
2020-12-29 21:41:11 +0000

    first commit

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -41,6 +41,7 @@ export const signup = (formData) => async dispatch => {
             payload: res.data.user
         })
     } catch(err) {
+        console.log(err.response)
         dispatch(setAlert(`${err.response.data.message}`, 'danger'))
     }
 }
