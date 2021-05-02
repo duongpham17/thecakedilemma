8a7c56180ec3556df37e6465f286eb4bad72ccec
tung duong <tung@tungs-MacBook-Pro.local>
2021-05-02 01:08:34 +0100

    new

--- server.js
+++ server.js
@@ -1,23 +1,17 @@
 const mongoose = require('mongoose')
 const app = require('./app');
 
-const dotenv = require('dotenv');
-dotenv.config({path: './config.env'});
-
 //connect to MongoDB using mongoose
-const db = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD);
-mongoose.connect( db, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-})
-.then(() => {
-    //when in development will display to console.
-    if (process.env.NODE_ENV === "development") {
-        console.log("DB connection successful!");
+(async () => {
+    const db = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD);
+    const use = {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false};
+    try{
+        await mongoose.connect( db , use )
+        if (process.env.NODE_ENV === "development") console.log("DB connection successful!");
+    } catch (err){
+        console.log("Could not connect to database")
     }
-});
+})();
 
 const port = process.env.PORT || 8000
 app.listen(port, () => console.log(`Listening on port ${port}`))
